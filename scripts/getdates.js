87630114878f5f3776521554ea00594c13825af4
honest.chris <hchristopher@byupathway.edu>
2026-01-15 11:30:10 +0100

    updated assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Display current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Display last modified date
+document.getElementById("lastModified").textContent =
+    "Last Modification: " + document.lastModified;
