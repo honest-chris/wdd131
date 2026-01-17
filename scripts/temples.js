f742ac0af77a9ec5833e132f291419799cac4530
honest.chris <hchristopher@byupathway.edu>
2026-01-17 20:16:50 +0100

    last update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,9 @@
 console.log("Temples JS loaded");
 
 // Footer dates
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("currentyear").textContent = 
+  new Date().getFullYear();
+
 document.getElementById("last-modified").textContent =
     "Last Modification: " + document.lastModified;
 
@@ -10,12 +11,8 @@ document.getElementById("last-modified").textContent =
 const menuButton = document.getElementById("menu-button");
 const navMenu = document.getElementById("nav-menu");
 
-menuButton.addEventListener("click", function () {
+menuButton.addEventListener("click", () => {
   navMenu.classList.toggle("open");
-
-  if (navMenu.classList.contains("open")) {
-    menuButton.textContent = "✖";
-  } else {
-    menuButton.textContent = "☰";
-  }
+  menuButton.textContent = 
+  navMenu.classList.contains("open") ? "X" :"☰";
 });
